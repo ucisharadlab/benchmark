faee8c8c25e636f6c45a0ebdc5c97fbdab131e59
Peeyush Gupta <peeyushg@uci.edu>
2017-10-06 08:11:41 -0700

    Added a medium size experiment case

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,14 +1,19 @@
 #python python/generate.py python/configs/config1.ini
 #mvn exec:java
 #cp reports/report.txt reports/report1.txt
-#cp -R results/results1
+#cp -R results results1
 
-python python/generate.py python/configs/config2.ini
-mvn exec:java
-cp reports/report.txt reports/report2.txt
-cp -R results/results2
+#python python/generate.py python/configs/config2.ini
+#mvn exec:java
+#cp reports/report.txt reports/report2.txt
+#cp -R results results2
 
-python python/generate.py python/configs/config3.ini
+#python python/generate.py python/configs/config3.ini
+#mvn exec:java
+#cp reports/report.txt reports/report3.txt
+#cp -R results results3
+
+python python/generate.py python/configs/config4.ini
 mvn exec:java
-cp reports/report.txt reports/report3.txt
-cp -R results/results3
+cp reports/report.txt reports/report4.txt
+cp -R results results4
