6b13cf408dce5f7e43d77e090da4359b894130e7
Peeyush Gupta <peeyushg@uci.edu>
2017-11-08 15:57:30 -0800

    Added configuration files for each database

--- scripts/start/cratedb.sh
+++ scripts/start/cratedb.sh
@@ -1,3 +1,4 @@
 echo  "Starting CrateDB ... \n\n"
+CRATE_HEAP_SIZE=2g
 echo benchmark | sudo -S service crate start
 sleep 10
--- scripts/start/mongodb.sh
+++ scripts/start/mongodb.sh
@@ -1,3 +1,3 @@
 echo  "Starting MongoDB ... \n\n"
 export PATH=$PATH:/home/benchmark/opt/mongodb/bin
-mongod --fork --logpath /tmp/mongodb.log -dbpath /home/benchmark/opt/mongodb/data/
+mongod --fork  --config /home/benchmark/benchmark/benchmark/configuration/mongodb/mongod1.conf --logpath /tmp/mongodb.log -dbpath /home/benchmark/opt/mongodb/data/
