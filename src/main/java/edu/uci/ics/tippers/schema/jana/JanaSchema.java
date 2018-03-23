1c5a4ef6c413c543ddabedc725a62241ee1dc7b2
Peeyush Gupta <peeyush.lnmit@gmail.com>
2018-03-22 22:50:56 -0700

    Implemented Jana schema creation file for Mapping1 (All non sensitive)

--- src/main/java/edu/uci/ics/tippers/schema/jana/JanaSchema.java
+++ src/main/java/edu/uci/ics/tippers/schema/jana/JanaSchema.java
@@ -1,16 +1,13 @@
 package edu.uci.ics.tippers.schema.jana;
 
-import edu.uci.ics.tippers.common.DataFiles;
 import edu.uci.ics.tippers.common.Database;
 import edu.uci.ics.tippers.connection.jana.JanaConnectionManager;
 import edu.uci.ics.tippers.exception.BenchmarkException;
 import edu.uci.ics.tippers.schema.BaseSchema;
-import edu.uci.ics.tippers.schema.asterixdb.AsterixDBSchema;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -18,8 +15,8 @@ public class JanaSchema extends BaseSchema {
 
     private JanaConnectionManager connectionManager;
     private JSONParser parser = new JSONParser();
-    private String CREATE_SCHEMA_FILE = "jana/schema/create.json";
-    private String DROP_SCHEMA_FILE = "jana/schema/drop.json";
+    private String CREATE_SCHEMA_FILE = "jana/schema/mapping%s/create.json";
+    private String DROP_SCHEMA_FILE = "jana/schema/mapping%s/drop.json";
 
 
     public JanaSchema(int mapping, String dataDir) {
@@ -36,7 +33,7 @@ public class JanaSchema extends BaseSchema {
     public void createSchema() throws BenchmarkException {
         try {
             JSONArray schemaJson = (JSONArray) parser.parse(new InputStreamReader(
-                    JanaSchema.class.getClassLoader().getResourceAsStream(CREATE_SCHEMA_FILE)));
+                    JanaSchema.class.getClassLoader().getResourceAsStream(String.format(CREATE_SCHEMA_FILE, mapping))));
             connectionManager.createRelations(schemaJson);
         } catch (IOException | ParseException e) {
             e.printStackTrace();
@@ -49,8 +46,8 @@ public class JanaSchema extends BaseSchema {
     public void dropSchema() throws BenchmarkException {
         try {
             JSONArray schemaJson = (JSONArray) parser.parse(new InputStreamReader(
-                    JanaSchema.class.getClassLoader().getResourceAsStream(DROP_SCHEMA_FILE)));
-            connectionManager.createRelations(schemaJson);
+                    JanaSchema.class.getClassLoader().getResourceAsStream(String.format(DROP_SCHEMA_FILE, mapping))));
+            connectionManager.dropRelations(schemaJson);
         } catch (IOException | ParseException e) {
             e.printStackTrace();
             throw new BenchmarkException("Error While Creating Schema");
