76fa88fde67e6d286eb4eaa15f219e1864c79fe2
Peeyush <peeyushg@uci.edu>
2018-09-27 15:18:35 -0700

    Added Missing Explain Query Placeholders

--- src/main/java/edu/uci/ics/tippers/query/cassandra/CassandraQueryManager.java
+++ src/main/java/edu/uci/ics/tippers/query/cassandra/CassandraQueryManager.java
@@ -76,4 +76,54 @@ public class CassandraQueryManager extends BaseQueryManager {
     public Duration runQuery10(Date startTime, Date endTime) throws BenchmarkException {
         return null;
     }
+
+    @Override
+    public Duration explainQuery1(String sensorId) throws BenchmarkException {
+        return null;
+    }
+
+    @Override
+    public Duration explainQuery2(String sensorTypeName, List<String> locationIds) throws BenchmarkException {
+        return null;
+    }
+
+    @Override
+    public Duration explainQuery3(String sensorId, Date startTime, Date endTime) throws BenchmarkException {
+        return null;
+    }
+
+    @Override
+    public Duration explainQuery4(List<String> sensorIds, Date startTime, Date endTime) throws BenchmarkException {
+        return null;
+    }
+
+    @Override
+    public Duration explainQuery5(String sensorTypeName, Date startTime, Date endTime, String payloadAttribute, Object startPayloadValue, Object endPayloadValue) throws BenchmarkException {
+        return null;
+    }
+
+    @Override
+    public Duration explainQuery6(List<String> sensorIds, Date startTime, Date endTime) throws BenchmarkException {
+        return null;
+    }
+
+    @Override
+    public Duration explainQuery7(String startLocation, String endLocation, Date date) throws BenchmarkException {
+        return null;
+    }
+
+    @Override
+    public Duration explainQuery8(String userId, Date date) throws BenchmarkException {
+        return null;
+    }
+
+    @Override
+    public Duration explainQuery9(String userId, String infraTypeName) throws BenchmarkException {
+        return null;
+    }
+
+    @Override
+    public Duration explainQuery10(Date startTime, Date endTime) throws BenchmarkException {
+        return null;
+    }
 }
